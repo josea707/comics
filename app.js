6a4f72512c54e6a922135872f2dc225b6351951a
Jose Delgado <josedelgado@Joses-Mac-mini.local>
2021-06-15 12:08:27 -0400

    deployment

--- app.js
+++ app.js
@@ -52,8 +52,6 @@ app.get('/comics/comicsNum/random', async function (req, res) {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-
 if (process.env.NODE_ENV === 'production') {
   // static folder
   app.use(express.static('client/build'));
@@ -61,3 +59,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server started!!'));
